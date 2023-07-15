914e6e7147b1d6bef48868a2984231ea1f743ba7
ojegit <ojegit@gmail.com>
2023-07-15 20:52:55 +0300

    upd 4

---
+++ app/src/main/java/com/example/oo_harjoitustyo/BattleArena.java
@@ -0,0 +1,14 @@
+package com.example.oo_harjoitustyo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BattleArena extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_battle_arena);
+    }
+}
